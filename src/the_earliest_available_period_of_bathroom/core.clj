9a4ce800cd524dec0d16a12932ac51a573237e5b
Yan Qian <qianyan@thoughtworks.com>
2016-09-10 22:36:27 +0800

    [refactor] use macro thread-last ->> to pipe

--- src/the_earliest_available_period_of_bathroom/core.clj
+++ src/the_earliest_available_period_of_bathroom/core.clj
@@ -2,13 +2,14 @@
 
 (defn the-earliest-available-recommand [rooms]
   (if (seq rooms)
-    (first (sort-by :time
-                    (map (fn [room]
-                           (let [{:keys [room-id periods]} room
-                                 available-periods (filter #(#{:available} (:status %)) periods)]
-                             (if (seq available-periods)
-                               (merge {:room-id room-id}
-                                      (select-keys (first available-periods) [:time]))
-                               :no-available-room)))
-                         rooms)))
+    (->> rooms
+         (map (fn [room]
+                (let [{:keys [room-id periods]} room
+                      available-periods (filter #(#{:available} (:status %)) periods)]
+                  (if (seq available-periods)
+                    (merge {:room-id room-id}
+                           (select-keys (first available-periods) [:time]))
+                    :no-available-room))))
+         (sort-by :time)
+         first)
     :no-available-room))
