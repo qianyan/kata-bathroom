9178e47474a0108463997c2082c041b128d4e718
Yan Qian <qianyan@thoughtworks.com>
2016-09-10 22:05:02 +0800

    one room with multiple available periods

--- src/the_earliest_available_period_of_bathroom/core.clj
+++ src/the_earliest_available_period_of_bathroom/core.clj
@@ -1,4 +1,7 @@
 (ns the-earliest-available-period-of-bathroom.core)
 
 (defn the-earliest-available-recommand [rooms]
-  {:room-id 1 :time "17:00-18:00"})
+  (let [{:keys [room-id periods]} (first rooms)
+        available-periods (filter #(#{:available} (:status %)) periods)]
+    (merge {:room-id room-id}
+           (select-keys (first available-periods) [:time]))))
--- test/the_earliest_available_period_of_bathroom/core_test.clj
+++ test/the_earliest_available_period_of_bathroom/core_test.clj
@@ -3,12 +3,28 @@
   (:use [the-earliest-available-period-of-bathroom.core]))
 
 (def room-1 {:room-id 1
-    :periods [{:time "17:00-18:00"
-               :status :available}
-              {:time "18:00-19:00"
-               :status :occupied}]})
+             :periods [{:time "17:00-18:00"
+                        :status :available}
+                       {:time "18:00-19:00"
+                        :status :occupied}]})
 
+(def room-2 {:room-id 2
+             :periods [{:time "17:00-18:00"
+                        :status :occupied}
+                       {:time "18:00-19:00"
+                        :status :available}]})
+
+(def room-3 {:room-id 3
+             :periods [{:time "17:00-18:00"
+                        :status :occupied}
+                       {:time "18:00-19:00"
+                        :status :available}
+                       {:time "19:00-20:00"
+                        :status :available}]})
 
 (facts "about `the-earliest-avaible-period-of-bathroom`"
-  (fact "should recommand if there is only one room with available period"
-    (the-earliest-available-recommand rooms) => {:room-id 1 :time "17:00-18:00"}))
+  (fact "should recommand the available if there is only one room with one available period"
+    (the-earliest-available-recommand [room-1]) => {:room-id 1 :time "17:00-18:00"}
+    (the-earliest-available-recommand [room-2]) => {:room-id 2 :time "18:00-19:00"})
+  (fact "should recommand the earliest one if there is only one room with multiple available periods"
+    (the-earliest-available-recommand [room-3]) => {:room-id 3 :time "18:00-19:00"}))
