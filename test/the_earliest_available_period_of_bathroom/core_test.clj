dd9335201b7a6cf1af3c0cef91089b45ce37b3ce
Yan Qian <qianyan@thoughtworks.com>
2016-09-13 10:28:06 +0800

    cleanup tests

--- test/the_earliest_available_period_of_bathroom/core_test.clj
+++ test/the_earliest_available_period_of_bathroom/core_test.clj
@@ -31,16 +31,11 @@
                         :status :occupied}]})
   
 (facts "about `the-earliest-avaible-period-of-bathroom`"
-  (fact "should recommand the available if there is only one room with one available period"
-    (the-earliest-available-recommand [room-1]) => {:room-id 1 :time "17:00-18:00"}
-    (the-earliest-available-recommand [room-2]) => {:room-id 2 :time "18:00-19:00"})
-  
   (fact "should recommand the earliest one if there is only one room with multiple available periods"
     (the-earliest-available-recommand [room-3]) => {:room-id 3 :time "18:00-19:00"})
   
   (fact "should show `:no-available-room` if there is no available room"
     (the-earliest-available-recommand []) => :no-available-room
-    (the-earliest-available-recommand [non-available-room]) => :no-available-room
     (the-earliest-available-recommand [non-available-room non-available-room]) => :no-available-room)
  
   (fact "should recommand the earliest if there has more than one room and each has available periods"
@@ -48,7 +43,4 @@
     (the-earliest-available-recommand [room-2 room-1]) => {:room-id 1 :time "17:00-18:00"}
     (the-earliest-available-recommand [room-2 room-3]) => {:room-id 2 :time "18:00-19:00"}
     (the-earliest-available-recommand [room-1 room-2 room-3]) => {:room-id 1 :time "17:00-18:00"})
-  
-  (fact "should recommand the earliest available room even if there has non available room"
-    (the-earliest-available-recommand [room-1 non-available-room]) => {:room-id 1 :time "17:00-18:00"}
-    (the-earliest-available-recommand [room-2 non-available-room]) => {:room-id 2 :time "18:00-19:00"}))
+    (the-earliest-available-recommand [room-2 non-available-room]) => {:room-id 2 :time "18:00-19:00"})
